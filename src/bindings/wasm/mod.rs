00d1772f19b670643c3eb1b117ebd5276927abf7
1BADragon <6611786+1BADragon@users.noreply.github.com>
2023-05-22 17:53:52 -0700

    Removed wee-alloc from project

--- src/bindings/wasm/mod.rs
+++ src/bindings/wasm/mod.rs
@@ -5,12 +5,6 @@ use serde::Serialize;
 use serde_json::Value;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
