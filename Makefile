a7225b03e6ede241164f2e9b7cf80d86f0290b84
matt <matt@cedarai.com>
2023-07-11 13:17:44 -0700

    Added the ident filter

--- Makefile
+++ Makefile
@@ -9,16 +9,20 @@ test:
 test-all: test
 	cargo test --no-default-features $(CARGO_ARGS)
 
+.env:
+	python3 -m venv .env
+	source .env/bin/activate && pip install maturin
+
 wasm-binding:
 	RUSTFLAGS="-C opt-level=s" wasm-pack build --target web --features wasm $(CARGO_ARGS)
 	
 wasm-binding-release:
 	RUSTFLAGS="-C opt-level=s" wasm-pack build --target web --release --features wasm $(CARGO_ARGS)
 
-python-binding:
+python-binding: .env
 	source .env/bin/activate && maturin build --features python $(CARGO_ARGS)
 	
-python-binding-release:
+python-binding-release: .env
 	source .env/bin/activate && maturin build --features python --release $(CARGO_ARGS)
 
 wasm-example: wasm-binding
---
+++ src/utils/ident_filter.rs
@@ -0,0 +1,37 @@
+use crate::BindContext;
+
+pub struct IdentFilterIter<'a> {
+    bindings: &'a BindContext,
+    iter: &'a mut dyn Iterator<Item = &'a str>,
+}
+
+impl<'a> IdentFilterIter<'a> {
+    pub fn new(
+        bindings: &'a BindContext,
+        iterable: &'a mut dyn Iterator<Item = &'a str>,
+    ) -> IdentFilterIter<'a> {
+        IdentFilterIter {
+            bindings,
+            iter: iterable,
+        }
+    }
+}
+
+impl<'a> Iterator for IdentFilterIter<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.iter.next() {
+                Some(val) => {
+                    if self.bindings.is_bound(val) {
+                        continue;
+                    }
+
+                    return Some(val.to_owned());
+                }
+                None => return None,
+            }
+        }
+    }
+}
